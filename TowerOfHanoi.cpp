73bf121b7cf00cf4614acb66d18f361e514b230d
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-09-09 23:15:07 +0530

    IMP for Interview

--- TowerOfHanoi.cpp
+++ TowerOfHanoi.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 void hanoi(int n, int src, int dest, int aux, vector<vector<int>> &ans){
   if(n == 1){
-      ans.push_back({src, dest});
-      return;
+    ans.push_back({src, dest});
+    return;
   }
 
+  // * Using the aux disk as destination and destination disk as aux
   hanoi(n-1, src, aux, dest, ans);
   ans.push_back({src,dest});
+  // * Using the dest disk as destination and aux disk as source
   hanoi(n-1, aux, dest, src, ans);
 }
 
